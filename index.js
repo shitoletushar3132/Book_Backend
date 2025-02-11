9305b6bc261e7e261107a149c1b5a950b9b5ed82
shitoletushar3132 <shitoletushar3132@gmail.com>
2025-02-11 11:45:19 +0530

    concurrency

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-const app = express();
-const router = require("./routers/index");
-const connectionDB = require("./config/connectionDB");
+const cluster = require("cluster");
 
-app.use(express.json());
-app.use(cors({origin:"*"}));
-app.use(express.urlencoded({ extended: true }));
-app.use("/api", router);
-const PORT = process.env.PORT || 5000; // Fallback to port 5000 if not defined in .env
+if (cluster.isMaster) {
+  const cpuCount = require("os").cpus().length;
 
-app.post("/add", (req, res) => {
-  console.log(req.body);
-});
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+} else {
+  const express = require("express");
+  require("dotenv").config();
+  const cors = require("cors");
+  const app = express();
+  const router = require("./routers/index");
+  const connectionDB = require("./config/connectionDB");
 
-connectionDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("Failed to connect to the database:", error.message);
+  app.use(express.json());
+  app.use(cors({ origin: "*" }));
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/api", router);
+  const PORT = process.env.PORT || 5000; // Fallback to port 5000 if not defined in .env
+
+  app.post("/add", (req, res) => {
+    console.log(req.body);
   });
+
+  connectionDB()
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log("Failed to connect to the database:", error.message);
+    });
+}
